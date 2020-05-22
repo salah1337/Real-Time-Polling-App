c3900964ea449001114a7d10c4214075b7f48909
salah1337 <salah1337@protonmail.com>
2020-05-22 02:33:07 +0000

    js update.

--- routes/poll.js
+++ routes/poll.js
@@ -5,12 +5,12 @@ var Pusher = require('pusher');
 var Vote = require('../models/Vote')
 
 var pusher = new Pusher({
-  appId: '1001995',
-  key: '39a0914e65feead5b9ed',
-  secret: '52227f6cde00c06140c8',
-  cluster: 'eu',
-  useTLS: true
-});
+    appId: 'yourinfo',
+    key: 'yourinfo',
+    secret: 'yourinfo',
+    cluster: 'yourinfo',
+    useTLS: true
+  });
 
 router.get('/', (req, res) => {
     Vote.find().then(votes => res.json({
