0da199f48be0ca6a9e30f4a40ddc4c7b6f6d00ce
salah1337 <salah1337@protonmail.com>
2020-05-22 02:32:16 +0000

    js update.

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://samhyde:mgtow1337@cluster0-yxb9t.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://username:password@cluster0-yxb9t.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('MongoDB succesfully connected'))
 .catch(err => console.log(err))
